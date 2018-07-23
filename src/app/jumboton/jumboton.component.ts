f047f6ef8e0448d7d65a1e3f7e551710bede2eec
Todsaporn Sangboon <nolifelover@gmail.com>
2018-07-23 15:31:25 +0700

    add four component for template

---
+++ src/app/jumboton/jumboton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jumboton',
+  templateUrl: './jumboton.component.html',
+  styleUrls: ['./jumboton.component.css']
+})
+export class JumbotonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
