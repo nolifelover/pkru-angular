3faaa416356443f6b735bb6f749a80988ee7a1e8
Todsaporn Sangboon <nolifelover@gmail.com>
2018-07-23 15:29:35 +0700

    using bootstrap and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotonComponent } from './jumboton/jumboton.component';
+import { BlogComponent } from './blog/blog.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    NavbarComponent,
+    JumbotonComponent,
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
